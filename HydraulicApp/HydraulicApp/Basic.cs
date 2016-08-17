6d31fbbfe2139529d34a93500cadf5c82b6eac51
steve simpson <stevesimpson4375@gmail.com>
2016-08-16 22:41:03 -0400

    Initial commit with some basics and a test form

---
+++ HydraulicApp/HydraulicApp/Basic.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HydraulicApp
+{
+    static class Basic
+    {
+        static public double FluidPressure(double force, double area)
+        {
+            return force / area;
+        }
+
+        static public double FluidFlowRate(double volume, double time)
+        {
+            return volume / time;
+        }
+
+        static public double Horsepower(double pressure, double flow)
+        {
+            return pressure * flow / 1714;
+        }
+
+        static public double Velocity(double GPM, double Area)
+        {
+            return 0.3208 * GPM / Area;
+        }
+
+        static public double Compressibility(double bulkModulus)
+        {
+            return 1 / bulkModulus;
+        }
+
+        static public double SpecificGravity(double weight)
+        {
+            return weight / 62.4283;
+        }
+    }
+}
---
+++ HydraulicApp/HydraulicApp/Pump.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HydraulicApp
+{
+    class Pump
+    {
+        public double Displacement { get; set; }
+        public double MaxPressure { get; set; }
+        public double Efficiency { get; set; }
+        public double MaxRPM { get; set; }
+
+        public Pump(double displacement, double maxPressure, double maxRPM)
+        {
+            Displacement = displacement;
+            MaxPressure = maxPressure;
+            MaxRPM = maxRPM;
+        }
+
+        public double PumpOutletFlow(double RPM)
+        {
+            return RPM * Displacement / 231;
+        }
+
+        public void DetermineEffiency(double outputPower, double inputPower)
+        {
+            Efficiency = outputPower / inputPower * 100; // Efficiency is measured as a percent value, hence the * 100
+        }
+
+        public void DeterminePumpDisplacement(double flow, double RPM)
+        {
+            Displacement = flow * 231 / RPM;
+        }
+
+        public double GetMaxPower()
+        {
+            return MaxPressure * Displacement / 1714;
+        }
+    }
+}
