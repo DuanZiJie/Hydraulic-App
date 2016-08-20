e2510ffd9f6f78f1dc8b9f890b578ef80b102219
steve simpson <stevesimpson4375@gmail.com>
2016-08-20 12:49:07 -0400

    Renamed Actuator.cs to Cylinder.cs. Actuators include motors, which should be separate to create objects of each type. The devices do not have overlapping methods.

---
+++ HydraulicApp/HydraulicApp/Cylinder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HydraulicApp
+{
+    class Cylinder
+    {
+        public static double CylinderAreaCapEnd(double diaOfPiston)
+        {
+            return Math.Pow(Math.PI * diaOfPiston, 2);
+        }
+
+        public static double CylinderAreaRodEnd(double diaOfPiston, double diaOfRod)
+        {
+            return Math.Pow((Math.PI * diaOfPiston), 2) - Math.Pow((Math.PI * diaOfRod),2);
+        }
+
+        public static double CylinderForce(double pressure, double area)
+        {
+            return pressure * area;
+        }
+        
+        public static double CylinderSpeed(double flowRate, double cylinderArea)
+        {
+            return (231 * flowRate) / (12 * 60 * cylinderArea);
+        }
+
+        public static double CyilnderVolume(double cylinderStroke, double area)
+        {
+            return cylinderStroke * area;
+        }
+
+        public static double CylinderFlowRate(double velocity, double area)
+        {
+            return (12 * 60 * velocity * area) / 231;
+        }
+    }
+}
