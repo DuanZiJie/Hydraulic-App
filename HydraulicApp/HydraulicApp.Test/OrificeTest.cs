1cbc9205637ae049786648ccaf7c92fdf984639c
steve simpson <stevesimpson4375@gmail.com>
2016-09-21 23:17:11 -0400

    Added some tests for the Orifice class. Cleaned up the method that determines CDValue.

---
+++ HydraulicApp/HydraulicApp.Test/OrificeTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace HydraulicApp.Test
+{
+    [TestFixture]
+    class OrificeTest
+    {
+        double testFlowrate = 5.0;
+        double testOrificeDia = .125;
+        string testOrificeShape = "Knife Edge";
+
+        public Orifice CreateTestOrifice()
+        {
+            Orifice testOrifice = new Orifice(testFlowrate, testOrificeDia, testOrificeShape);
+
+            return testOrifice;
+        }
+
+        [Test]
+        public void ShouldReturnCDValue()
+        {
+            Assert.That(CreateTestOrifice().CDValue, Is.EqualTo(.62));
+        }
+        [Test]
+        public void ShouldDeterminePressureDrop()
+        {
+            Assert.That(Orifice.SingleOrificePressureDrop(CreateTestOrifice()), Is.EqualTo(239.81846151840664755622591284611));
+        }
+    }
+}
--- HydraulicApp/HydraulicApp/Orifice.cs
+++ HydraulicApp/HydraulicApp/Orifice.cs
@@ -6,32 +6,31 @@ using System.Threading.Tasks;
 
 namespace HydraulicApp
 {
-    class Orifice
+    public class Orifice
     {
-        static double SpecificGravity = 0.8; // This value is only for hydraulic fluid.
-        double GPM { get; set; }
-        double Flowrate { get; set; }
-        double PressureDrop { get; set; }
-        double OrificeDia { get; set; }
-        double CDValue { get; set; }
-        string CDValueString { get; set; }
+        static double SpecificGravity = 0.8; // This value is only for hydraulic fluid, but so is this program.
+        public double Flowrate { get; set; }
+        public double PressureDrop { get; set; }
+        public double OrificeDia { get; set; }
+        public double CDValue { get; set; }
+        public string CDValueString { get; set; }
 
         static double[] OrificeValues = { .62, .98, .61, .82, .97, .54, .72 };
-        static string[] OrificeDescriptions = { "KnifeEdge" , "Rounded" , "ShortL", "LongL" , "RoundedL", "InletShortL", "InletLongL" };
+        static string[] OrificeDescriptions = { "Knife Edge" , "Rounded" , "Short L", "Long L" , "Rounded L", "Inlet Short L", "Inlet Long L" };
 
         public Orifice()
         {
 
         }
 
-        public Orifice(double GPM, double Flowrate, double OrificeDia, string CDValueString)
+        public Orifice(double Flowrate, double OrificeDia, string CDValueString)
         {
-            this.GPM = GPM;
             this.Flowrate = Flowrate;
             this.OrificeDia = OrificeDia;
+            this.CDValueString = CDValueString;
             try
             {
-                this.DetermineCDValue(this);
+                this.CDValue = this.ReturnCDValue();
             }
             catch (Exception e)
             {
@@ -39,23 +38,11 @@ namespace HydraulicApp
             }
         }
 
-        public void DetermineCDValue(Orifice orifice)
-        {
-            try
-            {
-                this.CDValue = ReturnCDValue(orifice);
-            }
-            catch (Exception e)
-            {
-                throw (e);
-            }
-        }
-
-        private double ReturnCDValue(Orifice orifice)
+        private double ReturnCDValue()
         {
             for(int x = 0; x < OrificeDescriptions.Length; ++x)
             {
-                if (orifice.CDValueString.Equals(OrificeDescriptions[x]))
+                if (this.CDValueString.Equals(OrificeDescriptions[x]))
                 {
                     return OrificeValues[x];
                 }
@@ -65,10 +52,8 @@ namespace HydraulicApp
         }
 
         static public double SingleOrificePressureDrop(Orifice orifice)
-        {
-            Orifice tempOrifice = orifice;
-            
-            return SpecificGravity * Math.Pow(tempOrifice.Flowrate / (29.81 * orifice.CDValue * Math.Pow(tempOrifice.OrificeDia, 2)), 2);
+        {            
+            return SpecificGravity * Math.Pow(orifice.Flowrate / (29.81 * orifice.CDValue * Math.Pow(orifice.OrificeDia, 2)), 2);
         }
     }
 }
